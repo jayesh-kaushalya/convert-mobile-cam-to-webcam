4ad668cf4643157a68bfbf9eb78a10565a1b8f42
jayesh <84486612+jayesh-kaushalya@users.noreply.github.com>
2022-09-28 20:38:07 +0530

    Add files via upload

---
+++ main_file.py
@@ -0,0 +1,13 @@
+import urllib.request
+import cv2
+import numpy as np
+import imutils
+url = "http://192.168.43.158:8080/shot.jpg"
+while True:
+    imgPath = urllib.request.urlopen(url)
+    imgNp = np.array(bytearray(imgPath.read()), dtype=np.uint8)
+    img = cv2.imdecode(imgNp, -1)
+    img = imutils.resize(img, width=900)
+    cv2.imshow("CameraFeed", img)
+    if ord('q') == cv2.waitKey(1):
+        exit(0)
